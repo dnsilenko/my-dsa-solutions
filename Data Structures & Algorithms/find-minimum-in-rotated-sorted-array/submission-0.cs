21ec6a6e1b2cae70e139682e3df899b7dc0ec6bb
dnsilenko <dnsbrawler222@gmail.com>
2026-06-15 16:39:35 +0300

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int l = 0, r = nums.Length - 1;
+        int res = nums[0];
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                return Math.Min(res, nums[l]);
+            }
+
+            int mid = (l + r) / 2;
+            res = Math.Min(res, nums[mid]);
+
+            if (nums[mid] >= nums[l]) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        return res;
+    }
+}
