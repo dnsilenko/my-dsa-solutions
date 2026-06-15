0c4a46d069318b9640dd0f4d85c9a787e81bdb13
dnsilenko <dnsbrawler222@gmail.com>
2026-06-15 13:35:25 +0300

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.cs
@@ -0,0 +1,40 @@
+public class Solution {         
+    public int MinEatingSpeed(int[] piles, int h) {
+
+        Array.Sort(piles);
+        int l = 1, r = piles[piles.Length - 1], mid = (l + r) / 2, lv = 0; 
+
+        while (l <= r) // від одиниці до максимального значення
+        {
+            bool validate = Validate(piles, h, mid);
+
+            if (validate) 
+            {
+                lv = mid; 
+                r = mid - 1;
+            }
+            else if (!validate)
+            {
+                l = mid + 1;
+            }
+          
+            mid = (l + r) / 2;
+            if (mid == 0) mid = 1;
+        }
+
+        if (Validate(piles, h, mid)) return mid;
+        else return lv; // last validate
+    }
+
+    private bool Validate(int[] piles, int h, int speed)
+    {
+        int counter = 0;
+        for (int i = 0; i < piles.Length; i++)
+        {
+            counter += (piles[i] + speed - 1) / speed;
+        }
+
+        if (counter <= h) return true;
+        return false;
+    }
+}
