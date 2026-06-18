2509310c54e8969030c8deac744115d204637e67
dnsilenko <dnsbrawler222@gmail.com>
2026-06-18 13:32:52 +0300

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        var dict = new Dictionary<char, int>();
+        int l = 0, r = 0, max = 0;
+        while (l < s.Length)
+        {
+            if (r < s.Length)
+            {
+                if (dict.ContainsKey(s[r])) dict[s[r]]++;
+                else dict[s[r]] = 1;
+            }
+
+            int count = r - l + 1 - GetTheMostPopular(dict);
+            if (count <= k && r < s.Length)
+            { 
+                if (r - l + 1 > max) max = r - l + 1;
+            }
+            else
+            {
+                dict[s[l]]--;
+                l++;
+            }
+
+            if (r < s.Length) r++;
+        }      
+
+        return max;
+    }
+
+    private int GetTheMostPopular(Dictionary<char, int> dict)
+    {
+        int max = 0;
+        foreach (var item in dict)
+        {
+            if (item.Value > max) max = item.Value;     
+        }
+
+        return max;
+    }
+}
