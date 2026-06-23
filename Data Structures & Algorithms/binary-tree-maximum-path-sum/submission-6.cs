da9707987002693f9df70ac3bac174fbc73f81e2
dnsilenko <dnsbrawler222@gmail.com>
2026-06-23 14:37:41 +0300

    Add: binary-tree-maximum-path-sum - submission-6

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-6.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MaxPathSum(TreeNode root) {
+
+        if (root is null) return 0;
+        else if (root.left is null && root.right is null) return root.val;
+
+        int sum = int.MinValue; DFS(root);
+        return sum;
+
+        int DFS(TreeNode node)
+        {
+            if (node is null) return 0;
+
+            int left = Math.Max(DFS(node.left), 0);
+            int right = Math.Max(DFS(node.right), 0);
+
+            int summary = 0;
+            if (node.val + left > node.val + right) summary = node.val + left;
+            else summary = node.val + right;   
+
+            if (sum < node.val + left + right) sum = node.val + left + right;
+            sum = Math.Max(sum, summary); 
+
+            return summary;
+        }
+    }
+}
