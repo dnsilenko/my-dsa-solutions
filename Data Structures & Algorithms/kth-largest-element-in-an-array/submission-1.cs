23b51c992731241dd0a98705449bb312c808d70b
dnsilenko <dnsbrawler222@gmail.com>
2026-06-29 12:59:13 +0300

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+
+        var minheap = new PriorityQueue<int, int>();       
+        for (int i = 0; i < nums.Length; i++)
+        {
+            minheap.Enqueue(nums[i], nums[i]);
+            if (minheap.Count > k) minheap.Dequeue();
+        }    
+
+        return minheap.Peek();
+    }
+}
