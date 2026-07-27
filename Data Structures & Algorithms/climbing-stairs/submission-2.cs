85505c6fe1742b5386def74c7f9e54d5218d56bb
dnsilenko <dnsbrawler222@gmail.com>
2026-07-27 14:52:44 +0300

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {     
+        var dp = new int[n + 1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < dp.Length; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[dp.Length - 1];                             
+    }
+}
