9a768040e04776b0bd38b96adc117c38a99cee2c
dnsilenko <dnsbrawler222@gmail.com>
2026-07-29 16:47:20 +0300

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0') return 0;
+        
+        var dp = new int[s.Length + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i < dp.Length; i++)        
+        {
+            if (s[i - 1] != '0') dp[i] = dp[i - 1];
+            
+            if (i - 2 < 0) continue; 
+            if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] < '7'))
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[dp.Length - 1];
+    }
+}
