923e67f7f74b3e356992c58e69b7bcdd151b12c3
dnsilenko <dnsbrawler222@gmail.com>
2026-07-29 15:07:19 +0300

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int CountSubstrings(string s)
+    {
+        int counter = s.Length;
+        for (int i = 0; i < s.Length; i++) 
+        {
+            var sb1 = new StringBuilder();
+
+            //case1:
+            for (int l = i - 1, r = i; l >= 0 && r < s.Length; l--, r++)
+            {
+                sb1.Insert(0, s[l]);
+                sb1.Append(s[r]);
+                if (IsPalindrome(sb1.ToString())) counter++;
+            }
+
+            var sb2 = new StringBuilder(); 
+            sb2.Append(s[i]);
+
+            // case2:
+            for (int l = i - 1, r = i + 1; l >= 0 && r < s.Length; l--, r++)
+            {
+                sb2.Insert(0, s[l]);
+                sb2.Append(s[r]);
+                if (IsPalindrome(sb2.ToString())) counter++;
+            }
+        }     
+
+        return counter;    
+    }
+
+    private bool IsPalindrome(string word)
+    {
+        for (int i = 0, j = word.Length - 1; i < j; i++, j--)
+            if (word[i] != word[j]) return false;
+
+        return true;
+    }
+}
