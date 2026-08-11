87384d74aef7cf57bbae3cda597d07f9215f16cd
dnsilenko <dnsbrawler222@gmail.com>
2026-08-11 10:18:24 +0300

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public int[] CountBits(int n)
+    {
+        var dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) 
+        {   
+            dp[i] = dp[i / 2] + (i & 1);
+        }
+        
+        return dp;          
+    }
+}
