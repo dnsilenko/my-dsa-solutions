232bbee1b5e19f2a487b4f4db4989f37bb6abd8f
dnsilenko <dnsbrawler222@gmail.com>
2026-09-04 10:58:11 +0300

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var result = new int[2];
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i]))
+            {
+                result[0] = dict[target - nums[i]];
+                result[1] = i;
+
+                break;
+            }
+            else dict[nums[i]] = i;
+        }
+
+        return result;
+    }
+}
