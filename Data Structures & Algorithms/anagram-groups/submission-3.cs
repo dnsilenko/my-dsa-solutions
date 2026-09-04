3a21fb74255c9fa8437a21fff49399ee09d2df4a
dnsilenko <dnsbrawler222@gmail.com>
2026-09-04 11:22:33 +0300

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,30 @@
+public class Solution
+{
+    public List<List<string>> GroupAnagrams(string[] strs)
+    {
+        var result = new List<List<string>>();  
+        var dict = new Dictionary<string, List<string>>();
+
+        foreach (string word in strs)
+        {
+            var counting = new int[26];
+            foreach (char ch in word) counting[ch - 'a']++;
+
+            var sb = new StringBuilder();
+            foreach (int count in counting) 
+            {
+                sb.Append(count);
+                sb.Append("-");
+            }
+
+            if (!dict.ContainsKey(sb.ToString()))
+                dict[sb.ToString()] = new List<string>();
+
+            dict[sb.ToString()].Add(word);
+        }
+
+        foreach (var group in dict.Keys) result.Add(dict[group]);
+
+        return result;
+    }
+}
